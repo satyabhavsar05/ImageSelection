f00003103505ad4b8371157c0ae2d6e24b336c34
satyabhavsar05 <satya.bhavsar@gmail.com>
2020-05-13 22:40:02 +0530

    initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,172 @@
+package com.example.myapplication
+
+import android.app.Activity
+import android.content.ContentUris
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.DocumentsContract
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import java.io.File
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val FINAL_TAKE_PHOTO = 1
+    private val FINAL_CHOOSE_PHOTO = 2
+    private var picture: ImageView? = null
+    private var imageUri: Uri? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val takePhoto: Button = findViewById(R.id.cameraButton)
+        val chooseFromAlbum: Button = findViewById(R.id.galleryButton)
+        picture = findViewById(R.id.picture)
+
+        takePhoto.setOnClickListener {
+            val outputImage = File(externalCacheDir, "output_image.jpg")
+            if (outputImage.exists()) {
+                outputImage.delete()
+            }
+            outputImage.createNewFile()
+            imageUri = if (Build.VERSION.SDK_INT >= 24) {
+                FileProvider.getUriForFile(this, "com.mobiledev.imageutils.fileprovider", outputImage)
+            } else {
+                Uri.fromFile(outputImage)
+            }
+
+            val intent = Intent("android.media.action.IMAGE_CAPTURE")
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+            startActivityForResult(intent, FINAL_TAKE_PHOTO)
+        }
+
+        chooseFromAlbum.setOnClickListener {
+            val checkSelfPermission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            if (checkSelfPermission != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
+            } else {
+                openAlbum()
+            }
+        }
+    }
+
+    private fun openAlbum() {
+        val intent = Intent("android.intent.action.GET_CONTENT")
+        intent.type = "image/*"
+        startActivityForResult(intent, FINAL_CHOOSE_PHOTO)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1 ->
+                if (grantResults.isNotEmpty() && grantResults.get(0) == PackageManager.PERMISSION_GRANTED) {
+                    openAlbum()
+                } else {
+                    Toast.makeText(this, "You deied the permission", Toast.LENGTH_SHORT).show()
+                }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            FINAL_TAKE_PHOTO ->
+                if (resultCode == Activity.RESULT_OK) {
+                    val bitmap = BitmapFactory.decodeStream(imageUri?.let {
+                        getContentResolver().openInputStream(
+                                it
+                        )
+                    })
+                    picture!!.setImageBitmap(bitmap)
+                }
+            FINAL_CHOOSE_PHOTO ->
+                if (resultCode == Activity.RESULT_OK) {
+                    if (Build.VERSION.SDK_INT >= 19) {
+//                         4.4 or more
+                        handleImageOnKitkat(data)
+                    } else {
+//                         Below 4.4
+                        handleImageBeforeKitkat(data)
+                    }
+                }
+        }
+    }
+
+
+    private fun handleImageOnKitkat(data: Intent?) {
+        var imagePath: String? = null
+        val uri = data!!.data
+        if (if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    DocumentsContract.isDocumentUri(this, uri)
+                } else {
+                    TODO("VERSION.SDK_INT < KITKAT")
+                }) {
+//             Uri of document type, deal with document id
+            val docId = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                DocumentsContract.getDocumentId(uri)
+            } else {
+                TODO("VERSION.SDK_INT < KITKAT")
+            }
+            if (uri != null) {
+                if ("com.android.providers.media.documents" == uri.authority) {
+                    val id = docId.split(":")[1]
+                    val selsetion = MediaStore.Images.Media._ID + "=" + id
+                    imagePath = imagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selsetion)
+                } else if ("com.android.providers.downloads.documents" == uri.authority) {
+                    val contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), java.lang.Long.valueOf(docId))
+                    imagePath = imagePath(contentUri, null)
+                }
+            }
+        } else if (uri != null) {
+            if ("content".equals(uri.scheme, ignoreCase = true)) {
+                //             Content type Uri is handled in the normal way
+                imagePath = imagePath(uri, null)
+            } else if ("file".equals(uri.scheme, ignoreCase = true)) {
+                imagePath = uri.path
+            }
+        }
+        Log.i("path",imagePath)
+        displayImage(imagePath)
+    }
+
+    // For     devices without 4.4, skip
+    private fun handleImageBeforeKitkat(data: Intent?) {}
+
+    private fun imagePath(uri: Uri?, selection: String?): String {
+        var path: String? = null
+//         Get the path through Uri and selection
+        val cursor = uri?.let { contentResolver.query(it, null, selection, null, null) }
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA))
+            }
+            cursor.close()
+        }
+        return path!!
+    }
+
+    private fun displayImage(imagePath: String?) {
+        if (imagePath != null) {
+            Log.i("here",imagePath)
+            val bitmap = BitmapFactory.decodeFile(imagePath)
+            picture?.setImageBitmap(bitmap)
+        } else {
+            Toast.makeText(this, "Failed to get image", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
